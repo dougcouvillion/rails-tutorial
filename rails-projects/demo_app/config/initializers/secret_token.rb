0ab73a1c082f5b1aff5dbf9617967dc88d7176e6
Doug Couvillion <doug@getitllc.com>
2015-01-25 11:04:04 -0500

    Added the rails generated files and the updated Gemfile.

---
+++ rails-projects/demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '975cb3d630c6a16b7978b8e2de181fe6a8e650f4b5d0453b7937ebac26ae68e24138946cae7e4b16d07681067e0d5f2b066544def4a91ff933c687dafcc7c965'
